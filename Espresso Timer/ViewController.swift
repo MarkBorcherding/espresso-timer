e1e7642fafa8152c70ccf820d5b44a0cd7d9b95f
Mark Borcherding <markborcherding@gmail.com>
2015-12-07 19:53:53 -0600

    Initial Commit

---
+++ Espresso Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Espresso Timer
+//
+//  Created by Mark Borcherding on 12/7/15.
+//  Copyright © 2015 Mark Borcherding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
